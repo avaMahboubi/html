f3d4701b514ca9e6a70143e6812c3ed83f938db4
avaMahboubi <53194263+avaMahboubi@users.noreply.github.com>
2019-08-12 12:30:03 -0700

    js 1

---
+++ index/js/main.js
@@ -0,0 +1,20 @@
+//get element by id example
+document.getElementById('formJS').innerHTML="this massage came from JavaScript";
+
+var date = new Date();
+//get element by id example
+document.getElementById('formJS').innerHTML=date;
+
+
+
+
+
+
+
+//alert example
+alert('now i have JS');
+
+
+
+//get elemant by classes example
+document.getElementsByClassName('myclass')[1].innerHTML="these are classes";
